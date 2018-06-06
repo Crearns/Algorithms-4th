234fec8d2dc02af17b51974c181e14dbaf5c9fd9
Crearns <qq5267439342016@163.com>
2018-06-06 17:37:06 +0800

    readme.md shell and merge

---
+++ chapter2/Merge.java
@@ -0,0 +1,66 @@
+package chapter2;
+
+import stdio.StdOut;
+
+public class Merge {
+	private static Comparable[] aux;
+	
+	public static void sort(Comparable[] a) {
+		aux = new Comparable[a.length];
+		sort(a, 0, a.length-1);
+	}
+	
+	public static void sort(Comparable[] a, int lo, int hi) {
+		if (hi <= lo) return;
+		int mid = lo + (hi - lo)/2;
+		sort(a, lo, mid);
+		sort(a, mid+1, hi);
+		merge(a, lo, mid, hi);
+
+	}
+	
+	private static void merge(Comparable[] a, int lo, int mid, int hi) {
+        int i = lo, j = mid+1;
+        
+        for(int k = lo; k <= hi; k++)
+        	aux[k] = a[k];
+        
+        for (int k = lo; k <= hi; k++)
+        	if		(i > mid)					a[k] = aux[j++];
+        	else if (j > hi )					a[k] = aux[i++];
+        	else if (less(aux[j], aux[i])) 		a[k] = aux[j++];
+        	else 								a[k] = aux[i++];
+    }
+	
+	private static boolean less(Comparable v, Comparable w) {
+		return v.compareTo(w) < 0;
+	}
+	
+	private static void exch(Comparable[] a, int i, int j) {
+		Comparable t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+	
+	private static void show(Comparable[] a) {
+		for (int i = 0; i < a.length; i++) {
+			StdOut.print(a[i] + " ");
+			StdOut.println();
+		}
+	}
+	
+	public static boolean isSorted(Comparable[] a) {
+		for (int i = 1; i < a.length; i++) {
+			if(less(a[i], a[i - 1]))
+				return false;
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		String [] a = {"3", "7", "2", "3", "1", "5", "2", "1", "8", "5", "1", "2"};
+		sort(a);
+		assert isSorted(a);
+		show(a);
+	}
+}
