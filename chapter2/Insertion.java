bd24c48263f5a364006567a8d67907e21281e04a
Crearns <qq5267439342016@163.com>
2018-06-07 16:02:06 +0800

    insertion

--- chapter2/Insertion.java
+++ chapter2/Insertion.java
@@ -1,5 +1,50 @@
 package chapter2;
 
+import stdio.In;
+import stdio.StdDraw;
+import stdio.StdOut;
+
 public class Insertion {
+	
+	public static void sort(Comparable[] a) {
+		int N = a.length;
+		for (int i = 0; i < N; i++) {
+			for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
+				exch(a, j, j-1);
+			}
+		}
+	}
+	
+	private static boolean less(Comparable v, Comparable w) {
+		return v.compareTo(w) < 0;
+	}
+	
+	private static void exch(Comparable[] a, int i, int j) {
+		Comparable t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+	
+	private static void show(Comparable[] a) {
+		for (int i = 0; i < a.length; i++) {
+			StdOut.print(a[i] + " ");
+			StdOut.println();
+		}
+	}
+	
+	public static boolean isSorted(Comparable[] a) {
+		for (int i = 1; i < a.length; i++) {
+			if(less(a[i], a[i - 1]))
+				return false;
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		String [] a = In.readStrings();
+		sort(a);
+		assert isSorted(a);
+		show(a);
+	}
 
 }
